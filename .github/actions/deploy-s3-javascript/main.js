902d3ebc6e1539b70b6bfa058d4f0503fd0f8294
Gary McCart <gwmccart3@gmail.com>
2025-12-29 12:16:10 -0500

    added output

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -11,11 +11,12 @@ function run(){
 
     // 2) Upload files
     const s3Uri = `s3://${bucket}`;
-    
+
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
     
-
-    core.notice('Hello from my custom Javascript Action!')
+    const websiteURL = `https://${bucket}.s3-website-${bucketRegion}.amazonaws.com`
+    
+    core.setOutput('websiteUrl', websiteURL);
 }
 
 run();
